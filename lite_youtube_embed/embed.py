ad2c4b694dbf1c15dbd71b0b85da9243bee993ff
Jake Howard <git@theorangeone.net>
2025-05-22 10:56:43 +0100

    Harden regex for extracting video id

--- lite_youtube_embed/embed.py
+++ lite_youtube_embed/embed.py
@@ -1,5 +1,5 @@
 import re
-from typing import Dict, List, Optional
+from typing import Any, Dict, List, Optional
 
 from django.utils.html import format_html
 from wagtail.embeds.finders.oembed import OEmbedFinder
@@ -13,7 +13,7 @@ class LiteYouTubeEmbedFinder(OEmbedFinder):
     https://github.com/paulirish/lite-youtube-embed
     """
 
-    EMBED_ID_RE = re.compile(r"\/embed\/(.*?)\?")
+    VIDEO_ID_RE = re.compile(r"\/embed\/([a-zA-Z0-9_-]+?)\?")
 
     def __init__(
         self, providers: Optional[List[Dict]] = None, options: Optional[Dict] = None
@@ -22,12 +22,12 @@ class LiteYouTubeEmbedFinder(OEmbedFinder):
 
     @classmethod
     def _get_video_id(cls, html: str) -> str:
-        matched = cls.EMBED_ID_RE.search(html)
+        matched = cls.VIDEO_ID_RE.search(html)
         if matched is None:
             raise ValueError(f"Unable to find video id in {html}")
         return matched.group(1)
 
-    def find_embed(self, *args: List, **kwargs: Dict) -> Dict:
+    def find_embed(self, *args: Any, **kwargs: Any) -> Dict:
         result = super().find_embed(*args, **kwargs)
         video_id = self._get_video_id(result["html"])
         result["html"] = format_html(
